04c3a51e514bdac17ab6a2b9086d2ee414b83359
Anderson Contreira (Melô) <anderson.contreira@gmail.com>
2023-03-15 09:54:46 -0300

    hurry update

--- scripts/git/config-modules.sh
+++ scripts/git/config-modules.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# TO LIST all the submodules execute
+# git config -f .gitmodules -l
 
 # serverless-node-template
-git config -f .gitmodules submodule.serverless-node-template.branch main
-git config -f .gitmodules submodule.serverless-node-template.shallow false
-git submodule update --init --remote serverless-node-template
+git config -f .gitmodules submodule.examples/node/serverless-node-template.branch main
+git config -f .gitmodules submodule.examples/node/serverless-node-template.shallow false
+git config -f .gitmodules submodule.examples/node/serverless-node-template.remote git@github.com:andersoncontreira/serverless-node-template.git
+#git submodule update --init --remote git@github.com:andersoncontreira/serverless-node-template.git
+# git submodule update --init --remote
 
 # serverless-python-template
-git config -f .gitmodules submodule.serverless-python-template.branch main
-git config -f .gitmodules submodule.serverless-python-template.shallow false
-git submodule update --init --remote serverless-python-template
+git config -f .gitmodules submodule.examples/python/serverless-python-template.branch main
+git config -f .gitmodules submodule.examples/python/serverless-python-template.shallow false
+#git config -f .gitmodules submodule.examples/python/serverless-python-template.remote git@github.com:andersoncontreira/serverless-python-template.git
+#git submodule update --init --remote git@github.com:andersoncontreira/serverless-python-template.git
+
+# serverless-python-template
+git config -f .gitmodules submodule.examples/node/serverless-lambda-api-node-template.branch main
+git config -f .gitmodules submodule.examples/node/serverless-lambda-api-node-template.shallow false
+#git config -f .gitmodules submodule.examples/node/serverless-lambda-api-node-template.remote git@github.com:andersoncontreira/serverless-lambda-api-node-template.git
+#git submodule update --init --remote git@github.com:andersoncontreira/serverless-lambda-api-node-template.git
+
+
+git submodule update --init --remote
--- scripts/git/gitmodules.sh
+++ scripts/git/gitmodules.sh
@@ -3,5 +3,8 @@
 # serverless-node-template
 git submodule add -b main git@github.com:andersoncontreira/serverless-node-template.git ./examples/node/serverless-node-template
 
+# serverless-lambda-api-node-template
+git submodule add -b main git@github.com:andersoncontreira/serverless-lambda-api-node-template.git ./examples/node/serverless-lambda-api-node-template
+
 # serverless-python-template
 git submodule add -b main git@github.com:andersoncontreira/serverless-python-template.git ./examples/python/serverless-python-template
